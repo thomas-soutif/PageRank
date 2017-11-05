95b2c192b2796f883bd61d11691eba021aeb8480
Soutif Thomas <th.soutif@gmail.com>
2017-11-05 10:56:34 +0100

    Creation of an graphic interface

--- src/main/java/PageRank.java
+++ src/main/java/PageRank.java
@@ -6,14 +6,30 @@ import Fonctions.ScoreRank;
 import java.util.Comparator;
 import java.util.Set;
 import java.util.TreeSet;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+
 
 /**
  * Created by s16005532 on 19/10/17.
  */
-public class PageRank {
+public class PageRank extends Application {
+
+
+    final GridPane grid = new GridPane();
     public static void main(String[] agrs){
 
 
+        launch(agrs); // lancer la fenetre 2D
         //La matrice qu'on test ici ([[0,0,1/3,0,0],[1/3,0,0,1/2,0],[0,1,0,0,1],[1/3,0,1/3,0,0],[1/3,0,1/3,1/2,0]]);
     double t[][] = new double[][]{
             {0,0,new Fraction(1,3).doubleValue(),0,0},
@@ -59,4 +75,38 @@ public class PageRank {
 
 
     }
+    @Override
+    public void start(Stage primaryStage){
+
+        Pane root = new Pane();
+
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+        addButton();
+
+        root.getChildren().add(grid);
+        Scene scene = new Scene(root,500,500);
+
+        primaryStage.setTitle("Page Rank");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
+
+    private void addButton() {
+
+        Button btn = new Button();
+        btn.setText("Trier selon la méthode PageRank");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent event) {
+                javafx.scene.text.Text buttonText = new javafx.scene.text.Text("A mettre ici");
+                grid.add(buttonText,0,0);
+
+            }
+        });
+        grid.add(btn,13,30);
+    }
+
 }
